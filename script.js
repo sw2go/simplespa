38e8308a3ae46b407ad6bfbb429655dbbf91ec69
aa <aac1291@gmx.ch>
2020-01-03 19:08:48 +0100

    some extensions like router and dynamic script fetching

--- script.js
+++ script.js
@@ -1,66 +1,115 @@
-documentReady(function() {
+var app = new Router({
+    onContentElementInserted: function(element) {
 
-    HttpGetRequest("/header.html").then(function(fetchedHtml) {
+        var count = 0;
 
-        // normalize browsers location, "/", "/contact", "/blog/blog1"
-        if (document.location.pathname.length > 1 &&  document.location.pathname.endsWith('/')) {
-            history.replaceState(null, "bla", document.location.pathname.slice(0,-1));
+        function loaded() {
+            count--;
+            if (count <= 0) {            
+                if (element.querySelector('pre[data-src]')) {
+                    window.Prism.fileHighlight();        
+                };
+            }
+        };
+
+        var elements = element.querySelectorAll('script[src]');
+        if (elements.length > 0) {
+            [].forEach.call(elements, function(element) {
+                var src = element.getAttribute("src");
+                if (!document.querySelector('head > script[src="' + src +  '"]' )) {
+                    var script = document.createElement('script');
+                    script.setAttribute('type', 'text/javascript');
+                    count++;
+                    script.onload = loaded;
+                    script.setAttribute('src', src);
+                    document.head.appendChild(script);                    
+                }            
+            });
         }
-        // insert Header
-        document.querySelector('#header').insertAdjacentHTML('beforeend', fetchedHtml);
+        else {
+            loaded();
+        }
 
-        // insert the current content
-        fetchContent(document.location.pathname);
+            
 
-        HttpGetRequest("/footer.html").then(function(html) {
-            document.querySelector('#footer').insertAdjacentHTML('beforeend', html);
-        })
-    });
 
-    window.onpopstate = function() {
-        this.fetchContent(document.location.pathname).then(function() {
-            this.displayContent(document.location.pathname);
-        });        
-    }    
+
+
+
+
+        
+
+
+
+
+
+    }
 });
 
-function fetchContent(path) {
-    return new Promise(function(resolve, reject) {
+
+function Router(config) {
+    
+    this.navigateToLink = function (event) {   
+        event.preventDefault();
+        fetchContent(event.target.pathname, function () {
+            history.pushState(null, document.title, event.target.pathname);
+            displayContent(event.target.pathname);
+        }, function(error){});
+    }
+
+    documentReady(function() {
+        // browser url-normalization: remove trailing "/" (on sub-pages only)
+        if (document.location.pathname.length > 1 &&  endsWith(document.location.pathname,'/')) {
+            history.replaceState(null, document.title, document.location.pathname.slice(0,-1));
+        }
+                
+        // insert the current content (based on the url)
+        fetchContent(document.location.pathname, function(html){},function(error){});
+
+        HttpGetRequest("/header.html", function(html) {
+            document.querySelector('#header').insertAdjacentHTML('beforeend', html);
+        }, function(error){});
+
+        HttpGetRequest("/footer.html", function(html) {
+            document.querySelector('#footer').insertAdjacentHTML('beforeend', html);
+        }, function(error) {});
+
+        window.onpopstate = function() {
+            this.fetchContent(document.location.pathname, function() {
+                this.displayContent(document.location.pathname);
+            }, function(error){});        
+        }    
+    });
+
+    function fetchContent(path, resolve, reject) {    
         if (document.getElementById(path)) {
             console.log('cache');
             resolve();
         } else {
-            fileUrl = path.endsWith('/') ? "/main.html" : path + '/content.html';
-            HttpGetRequest(fileUrl).then(function(html) {
+            fileUrl = endsWith(path,'/') ? "/main.html" : path + '/content.html';
+            HttpGetRequest(fileUrl, function(html) {
                 console.log("load");
                 let content = document.querySelector('#content');
                 content.insertAdjacentHTML('beforeend', html);
                 content.lastElementChild.id = path;
+                if(config.onContentElementInserted) {
+                    config.onContentElementInserted(content.lastElementChild);
+                }          
                 resolve();
-            });            
+            }, function(error) {});            
         }
-    });
-}
+    }
 
-function navigateToLink(event) {   
-    event.preventDefault();
-    fetchContent(event.target.pathname).then(function () {
-        history.pushState(null, event.target.text, event.target.pathname);
-        displayContent(event.target.pathname);
-    });
-}
+    function displayContent(path) { // [].forEach for IE
+        [].forEach.call(document.querySelector('#content').childNodes,function(node) {
+            if (node instanceof Element) {
+                node.style.display = 'none';
+            }                    
+        });  
+        document.getElementById(path).style.display = 'block';
+    }
 
-function displayContent(path) { // [].forEach for IE
-    [].forEach.call(document.querySelector('#content').childNodes,function(node) {
-        if (node instanceof Element) {
-            node.style.display = 'none';
-        }                    
-    });  
-    document.getElementById(path).style.display = 'block';
-}
-
-function HttpGetRequest(path) {
-    return new Promise(function(resolve, reject){
+    function HttpGetRequest(path, resolve, reject) {
         var request = new XMLHttpRequest();
         request.open('GET', path, true); 
         request.onload = function() {
@@ -69,18 +118,21 @@ function HttpGetRequest(path) {
             } else {
                 reject(request.status + " " + request.statusText);
             }
-          };
+        };
         request.send();
-    });
-}
-
-function documentReady(fn) {
-    // see if DOM is already available, since IE9 and all chrome, safari etc. 
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-        // call on next available tick
-        setTimeout(fn, 1);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
     }
-} 
 
+    function documentReady(fn) {
+        // see if DOM is already available, since IE9 and all chrome, safari etc. 
+        if (document.readyState === "complete" || document.readyState === "interactive") {        
+            setTimeout(fn, 1);  // call on next available tick
+        } else {
+            document.addEventListener("DOMContentLoaded", fn);
+        }
+    } 
+
+    function endsWith(text, match) {    // for IE
+        var idx = text.lastIndexOf(match);
+        return (idx >=0 && idx === text.length - match.length);    
+    }
+}
